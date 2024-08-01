335c84c49017631e890cad1bf1ddb94c7595aa0f
Tutitta <111702644+SoyTutta@users.noreply.github.com>
2024-07-31 23:59:53 -0300

    AAA x3

--- src/main/java/com/soytutta/mynethersdelight/core/mixin/FrogEntityMixin.java
+++ src/main/java/com/soytutta/mynethersdelight/core/mixin/FrogEntityMixin.java
@@ -4,17 +4,16 @@ import com.soytutta.mynethersdelight.common.entity.ia.EatMagmaCakeGoal;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.ai.goal.GoalSelector;
 import net.minecraft.world.entity.animal.frog.Frog;
+import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.logging.Level;
-
 @Mixin(Frog.class)
 public abstract class FrogEntityMixin {
 
-    @Inject(method = "<init>", at = @At("TAIL"))
+    @Inject(method = "<init>(Lnet/minecraft/world/entity/EntityType;Lnet/minecraft/world/level/Level;)V", at = @At("TAIL"))
     private void onInit(EntityType<? extends Frog> entityType, Level level, CallbackInfo ci) {
         Frog frog = (Frog) (Object) this;
         GoalSelector goalSelector = frog.goalSelector;
